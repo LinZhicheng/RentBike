019e439a1b39475635f57abd2de013fabeade5fc
David Lin <linzhicheng1019@live.cn>
2016-05-29 19:27:17 +0800

    update portrait

--- app/src/main/java/com/android/davidlin/rentbike/view/MainMyProfileFragment.java
+++ app/src/main/java/com/android/davidlin/rentbike/view/MainMyProfileFragment.java
@@ -1,10 +1,16 @@
 package com.android.davidlin.rentbike.view;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,8 +21,15 @@ import com.android.davidlin.rentbike.R;
 import com.android.davidlin.rentbike.RentBike;
 import com.android.davidlin.rentbike.usercontrol.CircleImageView;
 import com.android.davidlin.rentbike.usercontrol.GroupTextView;
+import com.android.davidlin.rentbike.utils.PermissionsUtils;
+import com.avos.avoscloud.AVException;
 import com.avos.avoscloud.AVFile;
 import com.avos.avoscloud.AVUser;
+import com.avos.avoscloud.SaveCallback;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.util.UUID;
 
 /**
  * A {@link android.support.v4.app.Fragment} for user profile
@@ -25,6 +38,11 @@ import com.avos.avoscloud.AVUser;
 public class MainMyProfileFragment extends Fragment {
 
     public static final int REQUEST_CHOOSE_ORDER_TYPE = 0;
+    public static final int REQUEST_CHOOSE_PICTURE_ACTION = 1;
+    public static final int REQUEST_CAMERA = 2;
+    public static final int REQUEST_ALBUM = 3;
+    public static final int REQUEST_CROP = 4;
+
     private static final String TAG = "MyProfileFragment";
     private View view;
     private CircleImageView portraitIv;
@@ -32,6 +50,9 @@ public class MainMyProfileFragment extends Fragment {
     private GroupTextView accountTv, orderTv, bikesTv, msgTv;
     private AVUser currentUser;
     private View.OnClickListener startLogin;
+
+    // 拍照时的存储路径
+    private String name = "", route = "";
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup group, Bundle savedInstanceState) {
@@ -60,7 +81,7 @@ public class MainMyProfileFragment extends Fragment {
         portraitIv.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // 选择照片
+                startChoosePicture();
             }
         });
 
@@ -89,6 +110,7 @@ public class MainMyProfileFragment extends Fragment {
                 String url = portrait.getUrl();
                 RentBike.mImageLoader.displayImage(url, portraitIv);
             }
+
             usernameTv.setText(currentUser.getUsername());
             usernameTv.setOnClickListener(null);
             accountTv.setOnClickListener(new View.OnClickListener() {
@@ -101,7 +123,7 @@ public class MainMyProfileFragment extends Fragment {
             orderTv.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    startActionChoose();
+                    startChooseRole();
                 }
             });
             bikesTv.setOnClickListener(new View.OnClickListener() {
@@ -138,14 +160,114 @@ public class MainMyProfileFragment extends Fragment {
                     return;
             }
             startActivity(intent);
+        } else if (requestCode == REQUEST_CHOOSE_PICTURE_ACTION) {
+            int action_code = data.getIntExtra(PictureActionChooseFragment.EXTRA_PICTURE_ACTION, -1);
+            switch (action_code) {
+                case PictureActionChooseFragment.CODE_CAMERA:
+                    if (Environment.MEDIA_MOUNTED.equals(Environment.getExternalStorageState())
+                            || !Environment.isExternalStorageRemovable()) {
+                        route = getActivity().getExternalFilesDir(null).getPath();
+                    } else {
+                        route = getActivity().getFilesDir().getPath();
+                    }
+                    name = UUID.randomUUID().toString();
+                    File dir = new File(route);
+                    if (!dir.exists()) dir.mkdirs();
+                    Intent intent_camera = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                    intent_camera.putExtra(MediaStore.EXTRA_OUTPUT,
+                            Uri.fromFile(new File(route + "/" + name)));
+                    intent_camera.putExtra("outputFormat", "JPEG");
+                    try {
+                        startActivityForResult(intent_camera, REQUEST_CAMERA);
+                    } catch (SecurityException se) {
+                        PermissionsUtils.verifyCameraPermission(getActivity());
+                        Toast.makeText(getActivity(), "调用相机失败，请确认权限后重试", Toast.LENGTH_LONG).show();
+                        Log.e(TAG, se.getMessage());
+                    }
+                    break;
+                case PictureActionChooseFragment.CODE_ALBUM:
+                    Intent intent_album = new Intent(Intent.ACTION_PICK,
+                            MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                    startActivityForResult(intent_album, REQUEST_ALBUM);
+                    break;
+                default:
+                    Toast.makeText(getActivity(), "ERROR", Toast.LENGTH_SHORT).show();
+            }
+        } else if (requestCode == REQUEST_CAMERA) {
+            if (!route.equals("") && !name.equals("")) {
+                Uri uri = Uri.fromFile(new File(route + "/" + name));
+                Intent intent = startCutPicture(uri);
+                startActivityForResult(intent, REQUEST_CROP);
+            }
+        } else if (requestCode == REQUEST_ALBUM) {
+            Uri uri = data.getData();
+            Intent intent = startCutPicture(uri);
+            startActivityForResult(intent, REQUEST_CROP);
+        } else if (requestCode == REQUEST_CROP) {
+            final Bitmap bmp = data.getParcelableExtra("data");
+            final ProgressDialog dialog = new ProgressDialog(getActivity());
+            dialog.setTitle("请稍等...");
+            dialog.setMessage("正在上传头像...");
+            dialog.show();
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            bmp.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+            byte[] bytes = baos.toByteArray();
+            final AVFile portrait = new AVFile("portrait", bytes);
+            portrait.saveInBackground(new SaveCallback() {
+                @Override
+                public void done(AVException e) {
+                    if (e == null) {
+                        final AVFile oldPortrait = currentUser.getAVFile("portrait");
+                        currentUser.put("portrait", portrait);
+                        currentUser.saveInBackground(new SaveCallback() {
+                            @Override
+                            public void done(AVException e) {
+                                if (e == null) {
+                                    dialog.dismiss();
+                                    portraitIv.setImageBitmap(bmp);
+                                    Toast.makeText(getActivity(), "头像设置成功", Toast.LENGTH_SHORT).show();
+                                    if (oldPortrait != null) {
+                                        oldPortrait.deleteInBackground();
+                                    }
+                                } else {
+                                    dialog.dismiss();
+                                    Toast.makeText(getActivity(), "头像设置失败", Toast.LENGTH_SHORT).show();
+                                    portrait.deleteInBackground();
+                                }
+                            }
+                        });
+                    } else {
+                        dialog.dismiss();
+                        Toast.makeText(getActivity(), "上传失败", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
         }
     }
 
-    private void startActionChoose() {
+    private void startChooseRole() {
         FragmentManager fm = getActivity().getSupportFragmentManager();
         OrderTypeChooseFragment fragment = new OrderTypeChooseFragment();
         fragment.setTargetFragment(MainMyProfileFragment.this, REQUEST_CHOOSE_ORDER_TYPE);
         fragment.show(fm, "请选择您的身份：");
     }
 
+    private void startChoosePicture() {
+        FragmentManager fm = getActivity().getSupportFragmentManager();
+        PictureActionChooseFragment fragment = new PictureActionChooseFragment();
+        fragment.setTargetFragment(MainMyProfileFragment.this, REQUEST_CHOOSE_PICTURE_ACTION);
+        fragment.show(fm, "从哪里获取图片:");
+    }
+
+    private Intent startCutPicture(Uri data) {
+        Intent intent = new Intent("com.android.camera.action.CROP");
+        intent.setDataAndType(data, "image/*");
+        intent.putExtra("crop", "true");
+        intent.putExtra("aspectX", 1);
+        intent.putExtra("aspectY", 1);
+        intent.putExtra("outputX", 200);
+        intent.putExtra("outputY", 200);
+        intent.putExtra("return-data", true);
+        return intent;
+    }
 }
